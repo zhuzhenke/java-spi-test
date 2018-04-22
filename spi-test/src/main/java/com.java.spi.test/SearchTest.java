f150074e0717bba13cf9daec2ce065d0d0bfa2ad
祝侦科 <zhuzhenke@raycloud.com>
2018-04-22 21:53:50 +0800

    META-INF services所在模块修改

--- spi-test/src/main/java/com.java.spi.test/SearchTest.java
+++ spi-test/src/main/java/com.java.spi.test/SearchTest.java
@@ -12,11 +12,18 @@ public class SearchTest {
 
     public static void main(String[] args) {
         //通过消除或修改为pom.xml文件中的spi-demo-a或spi-demo-b即可见到效果
-        Search search = SearchFactory.newSearch();
-        if (search != null) {
-            System.out.println(search.getClass());
-            List result = search.search("sss");
-            System.out.println(result.get(0));
+        while(true){
+            Search search = SearchFactory.newSearch();
+            if (search != null) {
+                System.out.println(search.getClass());
+                List result = search.search("sss");
+                System.out.println(result.get(0));
+            }
+            try {
+                Thread.sleep(100L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
